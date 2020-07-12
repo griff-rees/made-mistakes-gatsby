55de0b72b18f613f78d5e695fcf6b81a0511fd63
Dr Griffith Rees <griff.rees@gmail.com>
2020-07-12 18:29:02 +0100

    Add basic customisation in config/site.js, favicon and cover photo.

--- config/site.js
+++ config/site.js
@@ -1,13 +1,13 @@
 module.exports = {
   // pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Made Mistakes', // Navigation and site title
-  titleAlt: 'Made Mistakes', // Title for schema.org JSONLD
-  description: 'Personal website of designer Michael Rose (@mmistakes).',
-  url: 'https://mademistakes.com', // Domain of your site. No trailing slash!
+  title: 'Griffith Rees', // Navigation and site title
+  titleAlt: 'Griffith Rees', // Title for schema.org JSONLD
+  description: 'Personal site artist and researcher Griffith Rees (@griff_rees).',
+  url: 'https://griffithrees.netlify.app', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   image: {
     // Used for SEO, relative to /static/ folder
-    src: '/images/made-mistakes-logo.png',
+    src: '/images/grIcon.png',
     width: 384,
     height: 384,
   },
@@ -21,20 +21,20 @@ module.exports = {
     'https://mm-staticman.herokuapp.com/v2/entry/mmistakes/made-mistakes-gatsby/master/comments',
 
   // JSONLD / Manifest
-  favicon: '/images/made-mistakes-logo.png', // Used for manifest favicon generation
-  shortName: 'Made Mistakes', // shortname for manifest. MUST be shorter than 12 characters
+  favicon: '/images/grIcon.png', // Used for manifest favicon generation
+  shortName: 'Griff Rees', // shortname for manifest. MUST be shorter than 12 characters
   author: {
     // Author for schema.org JSONLD
-    name: 'Michael Rose',
-    url: 'https://mademistakes.com',
+    name: 'Griffith Rees',
+    url: 'https://griffithrees.org',
   },
   themeColor: '#ffffff',
   backgroundColor: '#111111',
 
-  twitter: '@mmistakes', // Twitter username
-  twitterUrl: 'https://twitter.com/mmistakes',
-  facebook: 'Made Mistakes', // Facebook site name
-  githubUrl: 'https://github.com/mmistakes',
+  twitter: '@griff_rees', // Twitter username
+  twitterUrl: 'https://twitter.com/griff_rees',
+  facebook: 'griffith.s.rees', // Facebook site name
+  githubUrl: 'https://github.com/griff_rees',
   instagramUrl: 'https://www.instagram.com/mmistakes/',
   feedUrl: '/atom.xml',
   githubApiToken: process.env.GITHUB_API_TOKEN,
@@ -78,5 +78,5 @@ module.exports = {
     siteKey: '6LdRBykTAAAAAFB46MnIu6ixuxwu9W1ihFF8G60Q',
     secret:
       'uK2DH+wELCxTtM0MmfKfT0W5GPt0B+dGVZ3L4IeYtnKRQ7a/zkdRUmi0z6J7K4BumST/CKuKnk4l3EUpDxWnA0E29kpMzbmhukm0vXCZfGg6zLETxcPXTWcrRchAp59oe77OdRlXjNe01nqL6fohmFv5lBT1SrxfORvBxvSsyTrrBXG1b5JJLleP9o4LGDK15lPCASEehWxpNTs+jxE/VUemMvedGautUBQyK5PRGaKJQb+xe9wl+9GgwueASRbGPl6yUnytUFy3V59yg+WbhbJJhA3dEKW4vnUu4jcgLL8FusCQQjDSvy0Ypqq3Dfx35L5YVfmLW+FrtYNvoD6zhA==',
-  },
+  }
 }
