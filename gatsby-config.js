b007da3c8e6d30c31cff51cee7aa6cb2b6e276dc
Dr Griffith Rees <griff.rees@gmail.com>
2020-08-02 19:41:06 +0100

    Remove trailing ')' in footnote around 'Making Mistakes'.

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     feedUrl: site.feedUrl,
 
     // Site config
-    copyrights: `&copy; 2019&mdash;${new Date().getFullYear()} <a href="https://griffithrees.com/humans.txt">Griffith Rees</a>. Some Rights Reserved.<br />Forked from <a href="https://github.com/mmistakes/made-mistakes-gatsby" rel="nofollow">Making Mistakes</a>)`,
+    copyrights: `&copy; 2019&mdash;${new Date().getFullYear()} <a href="https://griffithrees.com/humans.txt">Griffith Rees</a>. Some Rights Reserved.<br />Forked from <a href="https://github.com/mmistakes/made-mistakes-gatsby" rel="nofollow">Making Mistakes</a>`,
     defaultTheme: 'light',
     postsPerPage: 10,
     mainMenu: [
